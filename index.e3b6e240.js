e0e7111b4be45f15746840ec8be0e073ad53e061
DanielOsadchenko <105806718+DanielOsadchenko@users.noreply.github.com>
2022-09-05 15:53:43 +0000

    Deploying to gh-pages from @ DanielOsadchenko/DetailingBySemenov@6ff2a949695b9596ffbd985cc223a906941bbbf1 🚀

---
+++ index.e3b6e240.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".open-menu-button"),n=document.querySelector(".navigation"),t=document.querySelector(".close-mobile-menu");e.addEventListener("click",(function(){n.classList.add("is-open")})),t.addEventListener("click",(function(){n.classList.remove("is-open")}))}();
+//# sourceMappingURL=index.e3b6e240.js.map
---
+++ index.ef175259.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".open-menu-button"),n=document.querySelector(".navigation"),t=document.querySelector(".close-mobile-menu");e.addEventListener("click",(function(){n.classList.add("is-open")})),t.addEventListener("click",(function(){n.classList.remove("is-open")}));
+//# sourceMappingURL=index.ef175259.js.map
